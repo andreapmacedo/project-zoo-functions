cb136311e35f51d1fe49c1efd9a52afabc5dfb9e
Andre Macedo <aa.andremacedo@gmail.com>
2022-03-16 15:24:12 -0300

    implementação do requisito 1 modificada

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,12 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+// function getSpeciesByIds(...ids) {
+function getSpeciesByIds(...ids) {
+  // console.log(species);
   return species.filter((element) => ids.includes(element.id));
 }
-// console.log((species[0].id, species[1].id, species[2].id, species[3].id));
+console.log(getSpeciesByIds(species[0].id, species[1].id, species[2].id, species[3].id));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '01422318-ca2d-46b8-b66c-3e9e188244ed');
+// console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
