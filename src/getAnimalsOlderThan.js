6f36542c661afdde0d577975eeb5d6461dca9f69
Andre Macedo <aa.andremacedo@gmail.com>
2022-03-16 18:00:41 -0300

    adiciona solução para o requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,9 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, minAge) {
+  // return species.find((specie) => specie.name === animal).residents.every((animalAge) => animalAge.age >= age);
   return species.find(({ name }) => name === animal).residents.every(({ age }) => age >= minAge);
+  // return species.find(({ name }) => name === animal).residents.every(({ age }) => age >= minAge);
 }
 // console.log(getAnimalsOlderThan('tigers', 2));
 
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,11 +2,19 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 // function getSpeciesByIds(...ids) {
+// function getSpeciesByIds(ids) {
+//   // console.log(species);
+//   return species.filter((element) => (element.id === ids));
+//   // return species.filter((element) => element.id === ids[1]);
+// }
+
 function getSpeciesByIds(...ids) {
   // console.log(species);
   return species.filter((element) => ids.includes(element.id));
+  // return species.filter((element) => element.id === ids[1]);
 }
 console.log(getSpeciesByIds(species[0].id, species[1].id, species[2].id, species[3].id));
+// console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '01422318-ca2d-46b8-b66c-3e9e188244ed');
 // console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
