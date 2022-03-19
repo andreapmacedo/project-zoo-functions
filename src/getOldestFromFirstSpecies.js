79469a93c31c996c7f9ab6814d90e4a4a5759f5b
Andre Macedo <aa.andremacedo@gmail.com>
2022-03-19 16:28:10 -0300

    corrige resolução requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,12 +4,12 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(selectedId) {
   const specieId = employees.find(({ id }) => id === selectedId).responsibleFor[0];
   const selectedSpecie = species.find(({ id }) => id === specieId).residents;
-  console.log(selectedSpecie);
+  // console.log(selectedSpecie);
   const oldestAnimal = selectedSpecie.reduce((acc, curr) => Math.max(acc, curr.age), 0);
-  console.log(oldestAnimal);
+  // console.log(oldestAnimal);
   const selectedAnimal = selectedSpecie.find(({ age }) => age === oldestAnimal);
   // const selectedAnimal = selectedSpecie.find((element) => element.age === oldestAnimal);
-  return selectedAnimal;
+  return [selectedAnimal.name, selectedAnimal.sex, selectedAnimal.age];
 }
 // console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
